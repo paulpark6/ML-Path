d16e30ae40bcce7f08f8df8117469d33a27fbed2
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-06-19 23:55:31 -0800

    added image

--- ml-pages/src/App.js
+++ ml-pages/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import profileImage from './images/NikeOffwhite'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+      My React App
+     </h1>
+     <h2>
+      Author: Paul Park
+     </h2>
+     <img src={profileImage} alt="profile-image"/>
     </div>
   );
 }
