60bf1c8910f259445ab2137bfa2d69e34b223df5
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-06-20 00:08:41 -0800

    Added Content

--- ml-pages/src/App.js
+++ ml-pages/src/App.js
@@ -11,7 +11,45 @@ function App() {
       Author: Paul Park
      </h2>
      <img src={profileImage} alt="profile-image"/>
+     <header className="background">
+        <h1 className='titleBox'>
+          <div className='titleText'>
+            Machine Learning Path Plan
+          </div>
+        </h1>
+        <h1 className='box1'>
+          <div className='content'>
+            {certifications}
+            Python<br/>
+            ML course<br/>
+            projects
+            Experience
+          </div>
+        </h1>
+        <h5 className='box2'>
+          <div className='content'>
+            For certifications: <br/>
+            there is  AI-900 and DP-900 exams for microsoft
+          </div>
+        </h5>
+        <h5 className='box3'>
+          <div className='content'>
+            For Python: 
+            check Kaggle, 
+            learn Pandas, 
+            learn NumPy
+          </div>
+        </h5>
+        <h5 className='box4'>
+          <div className='content'>
+            Simple Project Idea: <br/>
+            First make program that reads .CSV files. The program should organize data with given discription.<br/>
+            Next make a program that predicts stock market prices based on historic data.                        
+          </div>
+        </h5>
+      </header>
     </div>
+    
   );
 }
 
